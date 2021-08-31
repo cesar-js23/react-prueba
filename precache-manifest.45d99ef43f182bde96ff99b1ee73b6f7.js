57eb56bb8481ca12015ae34b4763ca44dbb7db9d
Cesar Julian Salazar <trabajos.cesar23@gmail.com>
2021-08-31 16:04:35 -0500

    Updates

---
+++ precache-manifest.45d99ef43f182bde96ff99b1ee73b6f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d82ec8a33cd3a6c70c",
+    "url": "/react-prueba/static/css/main.d56b6d28.chunk.css"
+  },
+  {
+    "revision": "f5d82ec8a33cd3a6c70c",
+    "url": "/react-prueba/static/js/main.f5d82ec8.chunk.js"
+  },
+  {
+    "revision": "4423370e4355487ec3f3",
+    "url": "/react-prueba/static/js/runtime~main.4423370e.js"
+  },
+  {
+    "revision": "5acb88f72e0b6842bedc",
+    "url": "/react-prueba/static/css/2.acea06bf.chunk.css"
+  },
+  {
+    "revision": "5acb88f72e0b6842bedc",
+    "url": "/react-prueba/static/js/2.5acb88f7.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-prueba/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-prueba/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-prueba/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-prueba/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-prueba/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "2cb96437447abd313b0ad870eb195c0d",
+    "url": "/react-prueba/index.html"
+  }
+];
